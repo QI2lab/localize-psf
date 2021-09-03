corrected bug in otf2psf() calculation of coordinates and moved tests that should have been included in this repo when it was split form earlier versions

fit_psf.py
@@ -81,7 +81,7 @@ def otf2psf(otf, dfs=1):
 
     shape = otf.shape
     drs = np.array([1 / (df * n) for df, n in zip(shape, dfs)])
-    coords = [fft.fftshift(fft.fftfreq(n, dr)) for n, dr in zip(shape, drs)]
+    coords = [fft.fftshift(fft.fftfreq(n, 1 / (dr * n))) for n, dr in zip(shape, drs)]
 
     psf = fft.fftshift(fft.ifftn(fft.ifftshift(otf))).real
 