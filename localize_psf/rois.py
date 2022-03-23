d5213d3d335486d9b496713c1c021b379eba9183
ptbrown1729 <ptbrown1729@gmail.com>
2022-03-22 18:22:33 -0700

    updated cut_roi() to work on broadcastable arrays and added some type annotations

--- localize_psf/rois.py
+++ localize_psf/rois.py
@@ -4,7 +4,7 @@ Tools for dealing with regions of interest (ROI's)
 import numpy as np
 
 
-def roi2global(coords_roi, roi):
+def roi2global(coords_roi: list[float], roi: list[int]):
     """
     Convert from ROI coordinates to global coordinates. i.e. if we have an array M then
     ROI(M)[c1, c2, ..., cn] = M[c1_full, c2_full, ..., cn_full]
@@ -21,7 +21,7 @@ def roi2global(coords_roi, roi):
     return coords_full
 
 
-def global2roi(coords_full, roi):
+def global2roi(coords_full: list[float], roi: list[int]):
     """
     Convert from global coordinates to ROI coordinates. i.e. if we have an array M, then
     M[c1, c2, ..., cn] = ROI(M)[c1_xform, c2_xform, ..., cn_xform]
@@ -39,7 +39,7 @@ def global2roi(coords_full, roi):
     return coords_roi
 
 
-def get_centered_roi(centers, sizes, min_vals=None, max_vals=None):
+def get_centered_roi(centers: list, sizes: list[int], min_vals: list[int] = None, max_vals: list[int] = None):
     """
     Get end points of an roi centered about centers (as close as possible) with length sizes.
     If the ROI size is odd, the ROI will be perfectly centered. Otherwise, the centering will
@@ -89,37 +89,45 @@ def get_centered_roi(centers, sizes, min_vals=None, max_vals=None):
     return roi
 
 
-def cut_roi(roi, arr, allow_broadcastable_arrays=True):
+def cut_roi(roi: list[int], arr: np.ndarray, axes: tuple[int] = None, allow_broadcastable_arrays: bool = True) -> np.ndarray:
     """
     Return region of interest from an array
 
     @param roi: [a0_start, a0_end, a1_start, a1_end, ..., am_start, am_end]
     @param arr: array, which must have dimension m or greater
-    @param bool allow_broadcastable_arrays: whether or not to accept arrays which have size 1 along some of the
+    @param axes: which axes are to be sliced by the ROI. Be default these are the last m axes of the array
      dimensions. If these are allowed, they will not be affected by the slicing operations but will remain unit size
+    @param allow_broadcastable_arrays: whether or not to accept arrays which have size 1 along some of the
     @return arr_roi: array roi
     """
     if not np.mod(len(roi), 2) == 0:
         raise ValueError("roi array length must be even")
 
-    if not len(roi) // 2 <= arr.ndim:
-        raise ValueError("roi was too large for given array")
+    nroi_dim = len(roi) // 2
+    if nroi_dim > arr.ndim:
+        raise ValueError("roi has dimension %d, which is too large for array of dimension %d" % (nroi_dim, arr.ndim))
+
+    # default to last nroi_dim axes of array
+    if axes is None:
+        axes = np.arange(-nroi_dim, 0)
+    axes[axes < 0] = axes[axes < 0] + arr.ndim
+
+    # base is entire array
+    slices = [slice(0, arr.shape[ii]) for ii in range(arr.ndim)]
+    # update whichever axes need updating
+    for ii, ax in enumerate(axes):
+        # get slices, unless array has unit size over this dimension and then we will assume is broadcasting ...
+        if allow_broadcastable_arrays and arr.shape[ax] == 1:
+            slices[ax] = slice(0, 1)
+        else:
+            slices[ax] = slice(roi[2*ii], roi[2*ii + 1])
+
+    return arr[tuple(slices)]
 
 
-    # get slices, unless array has unit size over this dimension and then we will assume is broadcasting ...
-    if allow_broadcastable_arrays:
-        arr_shapes = arr.shape
-        slices = tuple([slice(roi[ii], roi[ii + 1]) if arr_shapes[ii//2] > 1 else slice(0, 1) for ii in range(0, len(roi), 2)])
-    else:
-        slices = tuple([slice(roi[ii], roi[ii + 1]) for ii in range(0, len(roi), 2)])
-
-
-    return arr[slices]
-
-
-def get_roi_size(sizes, dc, dz, ensure_odd=True):
+def get_roi_size(sizes: list[float], dc: float, dz: float, ensure_odd: bool = True):
     """
-    Get ROI size in pixels given a set of sizes in real units
+    Get closest larger ROI size in pixels given a set of sizes in real units
 
     @param sizes: [sz, sy, sx], ROI sizes in real units
     @param dc: pixel size
