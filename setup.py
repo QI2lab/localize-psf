162b51a27d8cb39697d3e50b775ddcfb736c23b1
AlexCoul <alexis.coullomb.pro@gmail.com>
2022-05-23 15:47:00 -0700

    Add test pipeline notebook

--- setup.py
+++ setup.py
@@ -15,16 +15,14 @@ required_pkgs = ['numpy',
 
 # optional required packages
 # e.g. `pip install -e .[gpu]`
-extras = {'gpu': ['cupy'], # todo: also need to include https://github.com/QI2lab/Gpufit with extras['gpu']
-          'psfmodels': ['psfmodels @ git+https://git@github.com/tlambert03/PSFmodels-py@master#egg=psfmodels'] # often have trouble installing this, so make optional
-          }
+extras = {'psfmodels': ['psfmodels @ git+https://git@github.com/tlambert03/PSFmodels-py@master#egg=psfmodels']} # often have trouble installing this, so make optional
 
 setup(
     name='localize_psf',
     version='0.1.0',
     description="A package for localizing diffraction limited spots in 3D microscopy data.",
     long_description=long_description,
-    author='qi2lab, Peter T. Brown',
+    author='qi2lab, Peter T. Brown, Alexis Coullomb',
     packages=find_packages(include=['localize_psf']),
     python_requires='>=3.7',
     install_requires=required_pkgs,
